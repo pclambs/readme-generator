540c7745f2fb35293fa4ee044c481d8037c88900
deepwubs <pchucklambs@gmail.com>
2023-08-02 23:57:37 -0500

    added questions

--- index.js
+++ index.js
@@ -1,9 +1,81 @@
 // TODO: Include packages needed for this application
 const fs = require('fs')
 const inquirer = require('inquirer')
+const generateMarkdown = require('./scripts/generate-markdown')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+inquirer.prompt(
+    [
+        {
+            type:'input',
+            name:'title',
+            message:'What is the title of your project?',
+            validate: (value) => { if (value) {return true} else {return 'Please enter a value.'}},
+        },
+        {
+            type:'input',
+            name:'motivation',
+            message:'What was your motivation to build this project?',
+            validate: (value) => { if (value) {return true} else {return 'Please enter a value.'}},
+        },
+        {
+            type:'input',
+            name:'why',
+            message:'Why did you build it? What problem does it solve?',
+            validate: (value) => { if (value) {return true} else {return 'Please enter a value.'}},
+        },
+        {
+            type:'input',
+            name:'learn',
+            message:'What did you learn?',
+            validate: (value) => { if (value) {return true} else {return 'Please enter a value.'}},
+        },
+        {
+            type:'checkbox',
+            name: 'license',
+            choices:['MIT', 'APACHE2.0', 'Boost1.0', 'MPL2.0', 'BSD2', 'BSD3', 'none'],
+            default: 'none',
+        },
+        {
+            type: 'input',
+            name: 'dependencies',
+            message: 'List any project dependencies here.',
+            validate: (value) => { if (value) {return true} else {return 'Please enter a value.'}},
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'List the languages or technologies associated with this project.',
+            validate: (value) => { if (value) {return true} else {return 'Please enter a value.'}},
+        },
+        {
+            type: 'input',
+            name: 'creator',
+            message: 'State your GitHub username?',
+            validate: (value) => { if (value) {return true} else {return 'Please enter a value.'}},
+        },
+        {
+            type: 'input',
+            name: 'name',
+            message: 'State your full name?',
+            validate: (value) => { if (value) {return true} else {return 'Please enter a value.'}},
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'State is your email address?',
+            validate: (value) => { if (value) {return true} else {return 'Please enter a value.'}},
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: 'List the GitHub usernames of any contributors to the project',
+            validate: (value) => { if (value) {return true} else {return 'Please enter a value.'}},
+        },
+    ]
+    )
+    
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
