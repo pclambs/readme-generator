4d4caf41736d96642f3a5a4709bd3057a81f4176
deepwubs <pchucklambs@gmail.com>
2023-08-03 01:30:50 -0500

    added readme layout to generateMarkdown.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Packages needed for this application
 import fs from 'fs'
 import inquirer from 'inquirer'
-import generateMarkdown from './utils/generateMarkdown'
+import { generateMarkdown } from './utils/generateMarkdown.js'
 
 // An array of questions for user input
 inquirer
@@ -10,76 +10,93 @@ inquirer
             type:'input',
             name:'title',
             message:'What is the title of your project?',
-            validate: (value) => { if (value) {return true} else {return 'Please enter a value.'}},
+            validate: (value) => { if (value) {return true} else {return 'Please input a value.'}},
         },
         {
             type:'input',
             name:'motivation',
             message:'What was your motivation to build this project?',
-            validate: (value) => { if (value) {return true} else {return 'Please enter a value.'}},
+            validate: (value) => { if (value) {return true} else {return 'Please input a value.'}},
         },
         {
             type:'input',
             name:'why',
-            message:'Why did you build it? What problem does it solve?',
-            validate: (value) => { if (value) {return true} else {return 'Please enter a value.'}},
+            message:'Why did you build it?',
+            validate: (value) => { if (value) {return true} else {return 'Please input a value.'}},
+        },
+        {
+            type:'input',
+            name:'what',
+            message:'What problem does it solve?',
+            validate: (value) => { if (value) {return true} else {return 'Please input a value.'}},
         },
         {
             type:'input',
             name:'learn',
             message:'What did you learn?',
-            validate: (value) => { if (value) {return true} else {return 'Please enter a value.'}},
-        },
-        {
-            type:'checkbox',
-            name: 'license',
-            choices:['MIT', 'APACHE2.0', 'Boost1.0', 'MPL2.0', 'BSD2', 'BSD3', 'none'],
-            default: 'none',
+            validate: (value) => { if (value) {return true} else {return 'Please input a value.'}},
         },
         {
             type: 'input',
             name: 'dependencies',
             message: 'List any project dependencies here.',
-            validate: (value) => { if (value) {return true} else {return 'Please enter a value.'}},
+            validate: (value) => { if (value) {return true} else {return 'Please input a value.'}},
         },
         {
-            type: 'input',
-            name: 'usage',
-            message: 'List the languages or technologies associated with this project.',
-            validate: (value) => { if (value) {return true} else {return 'Please enter a value.'}},
+            type:'input',
+            name:'install',
+            message:'How do you install your project?',
+            validate: (value) => { if (value) {return true} else {return 'Please input a value.'}},
+        },
+        {
+            type:'input',
+            name:'use',
+            message:"Provide instructions and examples for use.",
+            validate: (value) => { if (value) {return true} else {return 'Please input a value.'}},
         },
         {
             type: 'input',
             name: 'creator',
             message: 'State your GitHub username?',
-            validate: (value) => { if (value) {return true} else {return 'Please enter a value.'}},
+            validate: (value) => { if (value) {return true} else {return 'Please input a value.'}},
         },
         {
             type: 'input',
             name: 'name',
             message: 'State your full name?',
-            validate: (value) => { if (value) {return true} else {return 'Please enter a value.'}},
+            validate: (value) => { if (value) {return true} else {return 'Please input a value.'}},
         },
         {
             type: 'input',
             name: 'email',
             message: 'State is your email address?',
-            validate: (value) => { if (value) {return true} else {return 'Please enter a value.'}},
+            validate: (value) => { if (value) {return true} else {return 'Please input a value.'}},
         },
         {
             type: 'input',
-            name: 'tests',
+            name: 'contributors',
             message: 'List the GitHub usernames of any contributors to the project',
-            validate: (value) => { if (value) {return true} else {return 'Please enter a value.'}},
+            validate: (value) => { if (value) {return true} else {return 'Please input a value.'}},
+        },
+        {
+            type:'checkbox',
+            name: 'license',
+            choices:['MIT', 'APACHE2.0', 'Boost1.0', 'MPL2.0', 'BSD2', 'BSD3', 'none'],
+            validate: (value) => { if (value) {return true} else {return 'Please input a value.'}},
         },
     ])
     .then(answers => {
         console.log(answers)
+        const md = generateMarkdown(answers)
+        
+        fs.writeFile(`./new-readme/${answers.title}.md`, md, err => {
+          if (err) {
+            throw err
+          }    
+          console.log(`${answers.title}.md saved!`)
+        })
     })
-
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+    .catch(error => console.log(error))
 
 // TODO: Create a function to initialize app
 function init() {}
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -10,12 +10,49 @@ function renderLicenseLink(license) {}
 // If there is no license, return an empty string
 function renderLicenseSection(license) {}
 
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `
-  # ${data.title}
+// Function to generate markdown for README
+export const generateMarkdown = ({title, motivation, why, what, learn, install, use, license, dependencies, creator, name, email, contributors}) => {
+  return `# ${title}
 
-`;
+## Description
+
+- ${motivation}
+- ${why}
+- ${what}
+- ${learn}
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Installation
+
+Dependencies:
+- ${dependencies}
+
+Installation instructions:
+- ${install}
+
+## Usage
+
+- ${use}
+
+## Credits
+
+- ${creator}
+- ${name}
+- ${email}
+- ${contributors}
+
+## License
+
+- ${license}
+
+## Badges
+
+
+  `
 }
-
-module.exports = generateMarkdown;
